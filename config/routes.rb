dfae3ad5bf34a7dbb2cc38cd0da39c98918b578a
vagrant <vagrant@precise32.(none)>
2014-05-31 09:36:09 +0000

    Administration product

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+
+  namespace :administration do
+    resources :products
+  end
+
+  resources :administration
+
+  # resources :administration
+  # get 'administration/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
