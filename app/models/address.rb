dc5a5a55df3658945f582fc813c2438122b3481e
Matteo Manchi <matteo.manchi@gmail.com>
2014-06-14 18:32:51 +0200

    Validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,9 @@
 class Address < ActiveRecord::Base
-     #validate :street,  presence: true
-     #validate :city,    presence: true
-     #validate :state,   presence: true
-     #validate :nation,  presence: true
-     #validate :zipcode, presence: true, numericality: {only_integer: true}
+    validates :street,  presence: true
+    validates :city,    presence: true
+    validates :state,   presence: true
+    validates :nation,  presence: true
+    validates :zipcode, presence: true, numericality: {only_integer: true}
 
     has_one :customer
     has_one :provider
